1a871be1a0d667a3cc83795d963d80f999ccf5d0
Braulio P <damianpulido1970@gmail.com>
2024-06-12 20:06:34 -0600

    Creacion de la operacion multiplicar

--- main.c
+++ main.c
@@ -2,6 +2,19 @@
 
 int main ()
 {
-    printf ("Bienvenido a la calculadora 2.0");
+    int num1,num2;
+
+    printf ("Bienvenido a la calculadora que MULTIPLICA");
+    printf ("\n Dame el primer numero a multiplicar: ");
+    scanf("%d",&num1);
+
+     printf ("\n Dame el segundo numero a multiplicar:");
+    scanf("%d",&num2);
+
+    int resultado = num1*num2;
+
+     printf ("\n El resultado de la multiplicacion es: %d",resultado);
     return 0;
+
 }
+
