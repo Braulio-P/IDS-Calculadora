23517888f474a87d19887fe0a10da6273d01ba72
Braulio P <damianpulido1970@gmail.com>
2024-06-14 11:23:36 -0600

    Cambio de MASTER a sumar

--- main.c
+++ main.c
@@ -4,17 +4,16 @@ int main ()
 {
     int num1,num2;
 
-    printf ("Bienvenido a la calculadora que MULTIPLICA");
-    printf ("\n Dame el primer numero a multiplicar: ");
+    printf ("Bienvenido a la calculadora que SUMA");
+    printf ("\n Dame el primer numero a sumar: ");
     scanf("%d",&num1);
 
-     printf ("\n Dame el segundo numero a multiplicar:");
+     printf ("\n Dame el segundo numero a sumar:");
     scanf("%d",&num2);
 
-    int resultado = num1*num2;
+    int resultado = num1+num2;
 
-     printf ("\n El resultado de la multiplicacion es: %d",resultado);
+     printf ("\n El resultado de la suma es: %d",resultado);
     return 0;
 
 }
-
